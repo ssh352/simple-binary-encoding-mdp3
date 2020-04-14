f484011d4e35564d698ac95c728e168b204e03f5
Jeffrey Tran <jeffrey.tran@gmail.com>
2020-04-14 16:35:14 -0500

    merged reading properties directly from property class

--- sbe-samples/src/main/java/uk/co/real_logic/sbe/read_cme_pcaps/properties/ReadPcapProperties.java
+++ sbe-samples/src/main/java/uk/co/real_logic/sbe/read_cme_pcaps/properties/ReadPcapProperties.java
@@ -2,11 +2,21 @@ package uk.co.real_logic.sbe.read_cme_pcaps.properties;
 
 import java.io.*;
 import java.net.URL;
+import java.nio.file.Paths;
 import java.util.Objects;
 import java.util.Properties;
 
 public class ReadPcapProperties {
-    public Properties get_properties(String fileName) {
+    public final String os_string;
+    public final String in_file;
+    public final String out_file;
+    public final String data_source;
+    public final String schema_file;
+    public final boolean run_short;
+    public final boolean write_to_file;
+
+
+    public ReadPcapProperties(String fileName) {
         Properties prop = new Properties();
             // the configuration file name
             InputStream is=null;
@@ -26,6 +36,13 @@ public class ReadPcapProperties {
             catch (IOException ex) {
 
             }
-            return prop;
+        os_string= prop.getProperty("reader.os");
+        in_file = Paths.get(prop.getProperty("reader.in_file")).toString();
+        out_file = Paths.get(prop.getProperty("reader.out_file")).toString();
+
+        schema_file=Paths.get(prop.getProperty("reader.schema_file")).toString();
+        run_short = Boolean.getBoolean(prop.getProperty("reader.run_short"));
+        write_to_file = Boolean.getBoolean(prop.getProperty("reader.write_to_file"));
+        data_source=prop.getProperty("reader.data_source");
     }
 }
