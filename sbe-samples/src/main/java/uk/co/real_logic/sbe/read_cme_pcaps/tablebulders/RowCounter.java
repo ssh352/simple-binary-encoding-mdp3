a1266127cf0c2249452db68dafdc71cf892c4bfc
Jeffrey Tran <jeffrey.tran@gmail.com>
2020-04-16 21:19:41 -0500

    added row count class, now going to replace it in code.. next will add sending time.

---
+++ sbe-samples/src/main/java/uk/co/real_logic/sbe/read_cme_pcaps/tablebulders/RowCounter.java
@@ -0,0 +1,23 @@
+package uk.co.real_logic.sbe.read_cme_pcaps.tablebulders;
+
+public class RowCounter {
+private int row_count;
+
+
+    public RowCounter() {
+        this.row_count = 0;
+    }
+
+    public int increment_row_count() {
+        this.row_count+=0;
+        return this.row_count;
+    }
+
+    public int get_row_count() {
+        return this.row_count;
+    }
+
+
+
+
+}
