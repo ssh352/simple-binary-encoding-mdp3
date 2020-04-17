a9463172d696bfd3d6f56229832636517408f99c
Jeffrey Tran <jeffrey.tran@gmail.com>
2020-04-17 15:51:32 -0500

    added template id and packet sequence number, need to fix row number to increment at every row. probably just after printing it.

--- sbe-samples/src/main/java/uk/co/real_logic/sbe/read_cme_pcaps/tablebulders/RowCounter.java
+++ sbe-samples/src/main/java/uk/co/real_logic/sbe/read_cme_pcaps/tablebulders/RowCounter.java
@@ -2,6 +2,35 @@ package uk.co.real_logic.sbe.read_cme_pcaps.tablebulders;
 
 public class RowCounter {
 private int row_count;
+private int packet_sequence_number;
+private int template_id;
+
+    public int getPacketSequenceNumber() {
+        return packet_sequence_number;
+    }
+
+    public void setPacketSequenceNumber(int packet_sequence_number) {
+        this.packet_sequence_number = packet_sequence_number;
+    }
+
+    public int getTemplateId() {
+        return template_id;
+    }
+
+    public void setTemplateId(int template_id) {
+        this.template_id = template_id;
+    }
+
+
+    public long getSending_time() {
+        return sending_time;
+    }
+
+    public void setSending_time(long sending_time) {
+        this.sending_time = sending_time;
+    }
+
+    private long sending_time;
 
 
     public RowCounter() {
@@ -9,14 +38,14 @@ private int row_count;
     }
 
     public int increment_row_count() {
-        this.row_count+=0;
+        this.row_count+=1;
         return this.row_count;
     }
+
 
     public int get_row_count() {
         return this.row_count;
     }
-
 
 
 
