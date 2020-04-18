5d6e45ed756054e7f01aca903d46adfc3fa195e7
Jeffrey Tran <jeffrey.tran@gmail.com>
2020-04-18 11:42:01 -0500

    moved all construction of messageprocessorparameters to class, taking only prop.
    Probably needs to be logically separated, but at least takes it out
    of root class

--- sbe-samples/src/main/java/uk/co/real_logic/sbe/read_cme_pcaps/readers/ReadPcaps.java
+++ sbe-samples/src/main/java/uk/co/real_logic/sbe/read_cme_pcaps/readers/ReadPcaps.java
@@ -1,69 +1,40 @@
 package uk.co.real_logic.sbe.read_cme_pcaps.readers;
 
 
-import org.agrona.concurrent.UnsafeBuffer;
 import uk.co.real_logic.sbe.ir.Ir;
-import uk.co.real_logic.sbe.ir.IrDecoder;
-import uk.co.real_logic.sbe.ir.IrEncoder;
 import uk.co.real_logic.sbe.ir.Token;
 import uk.co.real_logic.sbe.otf.OtfHeaderDecoder;
 import uk.co.real_logic.sbe.otf.OtfMessageDecoder;
 import uk.co.real_logic.sbe.otf.TokenListener;
 import uk.co.real_logic.sbe.read_cme_pcaps.MessageProcessorInputs;
-import uk.co.real_logic.sbe.read_cme_pcaps.properties.DataOffsets;
 import uk.co.real_logic.sbe.read_cme_pcaps.properties.ReadPcapProperties;
 import uk.co.real_logic.sbe.read_cme_pcaps.stream_managers.PcapBufferManager;
 import uk.co.real_logic.sbe.read_cme_pcaps.tablebulders.RowCounter;
 import uk.co.real_logic.sbe.read_cme_pcaps.token_listeners.CompactTokenListener;
-import uk.co.real_logic.sbe.xml.IrGenerator;
-import uk.co.real_logic.sbe.xml.MessageSchema;
-import uk.co.real_logic.sbe.xml.ParserOptions;
-import uk.co.real_logic.sbe.xml.XmlSchemaParser;
 
-import java.io.*;
-import java.nio.ByteBuffer;
-import java.nio.MappedByteBuffer;
-import java.nio.channels.FileChannel;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
 import java.util.List;
 
 
 public class ReadPcaps {
-    private static final int SCHEMA_BUFFER_CAPACITY = 5000000 * 1024;
 
     public static void main(String[] args) throws Exception {
 
         ReadPcapProperties prop = new ReadPcapProperties(args[0]);
-        RowCounter row_counter = new RowCounter();
-        DataOffsets offsets = new DataOffsets(prop.data_source);
-
         Writer outWriter = getWriter(prop);
 
-        ByteBuffer encodedSchemaBuffer = ByteBuffer.allocateDirect(SCHEMA_BUFFER_CAPACITY);
-        encodeSchema(encodedSchemaBuffer, prop.schema_file);
-        encodedSchemaBuffer.flip();
-        Ir ir = decodeIr(encodedSchemaBuffer);
-        OtfHeaderDecoder headerDecoder = new OtfHeaderDecoder(ir.headerStructure()); // todo make obect that initializes both header decoder and ir decoder
+        MessageProcessorInputs message_processor_inputs = new MessageProcessorInputs(prop);
 
-        PcapBufferManager bufferManager = initializeBufferManager(prop, offsets); //todo put this function in constructor of buffer manager
-        MessageProcessorInputs message_processor_inputs = new MessageProcessorInputs(row_counter, headerDecoder, ir, bufferManager);
         processMessages(message_processor_inputs, outWriter);
-//        processMessages(, outWriter, ir, headerDecoder, bufferManager);
+
         outWriter.close();
     }
 
-    private static PcapBufferManager initializeBufferManager(ReadPcapProperties prop, DataOffsets offsets) throws IOException {
-        RandomAccessFile aFile = new RandomAccessFile(prop.in_file, "rw");
-        FileChannel inChannel = aFile.getChannel();
-        MappedByteBuffer encodedMsgBuffer = inChannel.map(FileChannel.MapMode.READ_ONLY, 0, inChannel.size());
-        encodedMsgBuffer.flip();  //make buffer ready for read
-        UnsafeBuffer buffer = new UnsafeBuffer(encodedMsgBuffer);
-        PcapBufferManager bufferManager = new PcapBufferManager(prop, offsets, buffer);
-        bufferManager.setBufferOffset(offsets.starting_offset); //skip leading bytes before message capture proper
-        return bufferManager;
-    }
 
     private static void processMessages(MessageProcessorInputs message_processor_inputs, Writer outWriter) throws IOException {
-        //private static void processMessages(RowCounter row_counter, Writer outWriter, Ir ir, OtfHeaderDecoder headerDecoder, PcapBufferManager bufferManager) throws IOException {
         RowCounter row_counter = message_processor_inputs.row_counter;
         OtfHeaderDecoder header_decoder = message_processor_inputs.header_decoder;
         PcapBufferManager pcap_buffer_manager = message_processor_inputs.pcap_buffer_manager;
@@ -123,24 +94,7 @@ public class ReadPcaps {
     }
 
 
-    private static void encodeSchema(ByteBuffer byteBuffer, final String schema_file) throws Exception {
-        final File initialFile = new File(schema_file);
-        final InputStream targetStream = new FileInputStream(initialFile);
-        try (final InputStream in = new BufferedInputStream(targetStream)) {
-            MessageSchema schema = XmlSchemaParser.parse(in, ParserOptions.DEFAULT);
-            Ir ir = new IrGenerator().generate(schema);
-            try (final IrEncoder irEncoder = new IrEncoder(byteBuffer, ir)) {
-                irEncoder.encode();
-            }
-        }
-    }
 
-
-    private static Ir decodeIr(ByteBuffer buffer) {
-        try (final IrDecoder irDecoder = new IrDecoder(buffer)) {
-            return irDecoder.decode();
-        }
-    }
 
 
 }
