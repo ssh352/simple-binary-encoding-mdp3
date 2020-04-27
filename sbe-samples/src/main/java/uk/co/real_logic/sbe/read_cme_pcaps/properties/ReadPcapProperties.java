d9695aabd4368d259737ddb4080b2818ea9775bc
Jeffrey Tran <jeffrey.tran@gmail.com>
2020-04-26 20:01:00 -0500

    added this to file, inlined parameters

--- sbe-samples/src/main/java/uk/co/real_logic/sbe/read_cme_pcaps/properties/ReadPcapProperties.java
+++ sbe-samples/src/main/java/uk/co/real_logic/sbe/read_cme_pcaps/properties/ReadPcapProperties.java
@@ -1,9 +1,10 @@
 package uk.co.real_logic.sbe.read_cme_pcaps.properties;
 
-import java.io.*;
-import java.net.URL;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Paths;
-import java.util.Objects;
 import java.util.Properties;
 
 public class ReadPcapProperties {
@@ -26,26 +27,24 @@ public class ReadPcapProperties {
 
 
             }
-            // load the properties file
-            try {
-                prop.load(is);
-                System.out.println(prop.getProperty("reader.name"));
-                // get the value for app.version key
-                System.out.println(prop.getProperty("reader.version"));
-            }
-            catch (IOException ex) {
+        // load the properties file
+        try {
+            prop.load(is);
+            System.out.println(prop.getProperty("reader.name"));
+            // get the value for app.version key
+            System.out.println(prop.getProperty("reader.version"));
+        } catch (IOException ex) {
 
-            }
-        os_string= prop.getProperty("reader.os");
-        in_file = Paths.get(prop.getProperty("reader.in_file")).toString();
-        out_file = Paths.get(prop.getProperty("reader.out_file")).toString();
+        }
+        this.os_string = prop.getProperty("reader.os");
+        this.in_file = Paths.get(prop.getProperty("reader.in_file")).toString();
+        this.out_file = Paths.get(prop.getProperty("reader.out_file")).toString();
 
-        schema_file=Paths.get(prop.getProperty("reader.schema_file")).toString();
-        run_short = Boolean.getBoolean(prop.getProperty("reader.run_short"));
-        String write_to_file_string=prop.getProperty("reader.write_to_file");
+        this.schema_file = Paths.get(prop.getProperty("reader.schema_file")).toString();
+        this.run_short = Boolean.getBoolean(prop.getProperty("reader.run_short"));
+        String write_to_file_string = prop.getProperty("reader.write_to_file");
 
-        Boolean write_to_file_bool = Boolean.parseBoolean(write_to_file_string);
-        write_to_file = write_to_file_bool;
-        data_source=prop.getProperty("reader.data_source");
+        this.write_to_file = Boolean.parseBoolean(write_to_file_string);
+        this.data_source = prop.getProperty("reader.data_source");
     }
 }
