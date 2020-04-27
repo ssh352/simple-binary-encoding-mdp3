8b209042221b0fc3a925bbf967e59193f7dd47a0
Jeffrey Tran <jeffrey.tran@gmail.com>
2020-04-26 21:05:44 -0500

    tried to move printing functions out of compact token listener

---
+++ sbe-samples/src/main/java/uk/co/real_logic/sbe/read_cme_pcaps/token_listeners/TokenOutput.java
@@ -0,0 +1,52 @@
+package uk.co.real_logic.sbe.read_cme_pcaps.token_listeners;
+
+import java.io.IOException;
+import java.io.Writer;
+
+public class TokenOutput {
+    private final CompactTokenListener compactTokenListener;
+    private final Writer out;
+    boolean include_value_labels;
+
+    public TokenOutput(CompactTokenListener compactTokenListener, Writer out, boolean include_value_labels) {
+        this.compactTokenListener = compactTokenListener;
+        this.out = out;
+        this.include_value_labels = include_value_labels;
+    }
+
+    public void writeRow(CompactTokenListener.RowType row_type, long message_count, long group_header_count, long group_element_count) {
+
+        writerOut(String.valueOf(message_count));
+        writerOut(", ");
+        writerOut(String.valueOf(group_header_count));
+        writerOut(", ");
+        writerOut(String.valueOf(group_element_count));
+        writerOut(", ");
+        writerOut(pad(row_type.toString(), 16, ' '));
+    }
+
+
+    void writerOut(String s) {
+        try {
+            this.out.write(s);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String pad(String str, int size, char padChar) {
+        StringBuffer padded = new StringBuffer(str);
+        while (padded.length() < size) {
+            padded.append(padChar);
+        }
+        return padded.toString();
+    }
+
+    public void printValue(String field_label, Object printableObject) {
+        this.writerOut(", ");
+        if (this.include_value_labels) {
+            this.writerOut(field_label);
+            this.writerOut("=");
+        }
+    }
+}
