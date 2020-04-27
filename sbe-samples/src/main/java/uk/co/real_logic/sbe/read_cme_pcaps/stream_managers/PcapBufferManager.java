eda92aeeffa5c91d7a95289fbb38fd0f5a8f6022
Jeffrey Tran <jeffrey.tran@gmail.com>
2020-04-26 19:55:04 -0500

    removed unneeded fields from buffer manager.. buffer manager does very little at this point.

--- sbe-samples/src/main/java/uk/co/real_logic/sbe/read_cme_pcaps/stream_managers/PcapBufferManager.java
+++ sbe-samples/src/main/java/uk/co/real_logic/sbe/read_cme_pcaps/stream_managers/PcapBufferManager.java
@@ -1,15 +1,11 @@
 package uk.co.real_logic.sbe.read_cme_pcaps.stream_managers;
 
 import org.agrona.concurrent.UnsafeBuffer;
-import uk.co.real_logic.sbe.read_cme_pcaps.properties.DataOffsets;
 
 public class PcapBufferManager {
     final public UnsafeBuffer buffer;
-    private int buffer_offset=0;
-    private int header_offset;
-    private int next_offset;
-    public PcapBufferManager(DataOffsets offsets, UnsafeBuffer buffer) {
-        this.buffer=buffer;
+    public PcapBufferManager(UnsafeBuffer buffer) {
+        this.buffer = buffer;
     }
 
     public UnsafeBuffer getBuffer(){
