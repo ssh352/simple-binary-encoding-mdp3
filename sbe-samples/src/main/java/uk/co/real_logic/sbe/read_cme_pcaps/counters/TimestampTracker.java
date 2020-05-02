a9ce08d9721003c73e089e715692638223d316c7
Jeffrey Tran <jeffrey.tran@gmail.com>
2020-05-02 07:04:01 -0500

    made but not used TimeStamp Tracker

---
+++ sbe-samples/src/main/java/uk/co/real_logic/sbe/read_cme_pcaps/counters/TimestampTracker.java
@@ -0,0 +1,34 @@
+package uk.co.real_logic.sbe.read_cme_pcaps.counters;
+
+import static org.junit.Assert.assertFalse;
+
+public class TimestampTracker {
+    long sending_time;
+    long transact_time;
+    boolean sending_time_set;
+    boolean transact_time_set;
+
+    public TimestampTracker() {
+    }
+
+    public long getSending_time() {
+        return this.sending_time;
+    }
+
+    public void setSending_time(long sending_time) {
+        this.sending_time = sending_time;
+        this.sending_time_set = true;
+    }
+
+    public long getTransact_time() {
+        return this.transact_time;
+    }
+
+    public void setTransact_time(long transact_time) {
+        assertFalse(transact_time_set);
+        this.transact_time_set = true;
+        this.transact_time = transact_time;
+    }
+
+
+}
