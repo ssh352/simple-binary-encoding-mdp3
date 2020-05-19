74fa8b73a75ad3849331f4394be6144c9139c99b
Jeffrey Tran <jeffrey.tran@gmail.com>
2020-05-18 22:03:45 -0500

    did simple fixes.
    going to look for unused classes to delete.
    
    todo.. compare complete directories.
    
    clean up read pcaps
    
    maybe make the scopeTracker more clear.

---
+++ codemr/workingtables/html/html_report_sbesbe-samplesmain/htmlx/modeldata/overview_metric_data.js
@@ -0,0 +1,1 @@
+var CodeMR_ProjectMetrics = {"Project Name":"sbe working set: {sbe.sbe-samples.main}", "Number of Highly Problematic Classes":0, "Number of Entities":20, "Number of Problematic Classes":0, "Class Lines of Code":754, "Number of External Packages":13, "Number of Packages":9, "Number of External Entities":55}
---
+++ codemr/workingtables/html/main_report/htmlx/modeldata/packagedependency.js
@@ -0,0 +1,29 @@
+var matrix = [[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[1,0,0,1,1,1,1,1,0],[2,1,0,0,0,1,0,2,0],[0,0,0,0,0,0,0,0,0],[0,0,0,2,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0]];
+var packages = [{
+"name": " uk.co.real_logic.sbe.read_cme_pcaps.counters", "color": " #3182bd"
+}
+,{
+"name": " uk.co.real_logic.sbe.read_cme_pcaps.token_listeners.decoders", "color": " #6baed6"
+}
+,{
+"name": " uk.co.real_logic.sbe.read_cme_pcaps.readers", "color": " #9ecae1"
+}
+,{
+"name": " uk.co.real_logic.sbe.read_cme_pcaps.token_listeners", "color": " #c6dbef"
+}
+,{
+"name": " uk.co.real_logic.sbe.tests", "color": " #e6550d"
+}
+,{
+"name": " uk.co.real_logic.sbe.read_cme_pcaps.TableOutputHandlers", "color": " #fd8d3c"
+}
+,{
+"name": " uk.co.real_logic.sbe.read_cme_pcaps.properties", "color": " #fdae6b"
+}
+,{
+"name": " uk.co.real_logic.sbe.read_cme_pcaps.PacketInfo", "color": " #fdd0a2"
+}
+,{
+"name": " uk.co.real_logic.sbe.read_cme_pcaps.stream_managers", "color": " #31a354"
+}
+];
