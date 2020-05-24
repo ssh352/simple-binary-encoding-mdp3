6a5912e45756a0c06db50d3ee4c9a0e7a981a92c
Jeffrey Tran <jeffrey.tran@gmail.com>
2020-05-24 12:40:31 -0500

    works with multiple files in directory, works with instrument definitions.

--- sbe-samples/src/main/java/uk/co/real_logic/sbe/read_cme_pcaps/TableOutputHandlers/TablesHandler.java
+++ sbe-samples/src/main/java/uk/co/real_logic/sbe/read_cme_pcaps/TableOutputHandlers/TablesHandler.java
@@ -8,54 +8,54 @@ import java.util.HashMap;
 import static uk.co.real_logic.sbe.read_cme_pcaps.token_listeners.ScopeLevel.*;
 
 public class TablesHandler {
-    final HashMap<String, SingleTableOutput> singleTablesOutput= new HashMap<>();
+    final HashMap<String, SingleTableOutput> singleTablesOutput = new HashMap<>();
     String currentTable;
     final String path;
     private final ScopeTracker scopeTracker;
 
 
     public TablesHandler(String path) throws IOException {
-        this.path=path;
-        this.scopeTracker=new ScopeTracker();
+        this.path = path;
+        this.scopeTracker = new ScopeTracker();
         this.addTable("packetheaders");
         this.addTable("messageheaders");
         this.addTable("groupheaders");
     }
 
-    public void addTable( String tableName) throws IOException {
-        if(!singleTablesOutput.containsKey(tableName)){
-        SingleTableOutput newTableOutput= new SingleTableOutput(this.path, tableName);
-        singleTablesOutput.put(tableName, newTableOutput) ;}
+    public void addTable(String tableName) throws IOException {
+        if (!singleTablesOutput.containsKey(tableName)) {
+            SingleTableOutput newTableOutput = new SingleTableOutput(this.path, tableName);
+            singleTablesOutput.put(tableName, newTableOutput);
+        }
     }
 
-    private void appendToTable(String columnName, String value){
+    private void appendToTable(String columnName, String value) {
         singleTablesOutput.get(this.currentTable).append(columnName, value);
     }
 
-    public  void completeRow(String tableName) throws IOException {
+    public void completeRow(String tableName) throws IOException {
         singleTablesOutput.get(tableName).completeRow();
     }
 
 
-
-    public void appendColumnValue(String columnName, String value){
-        switch(scopeTracker.getScopeLevel()){
+    public void appendColumnValue(String columnName, String value) {
+        switch (scopeTracker.getScopeLevel()) {
             case PACKET_HEADER:
-                this.currentTable="packetheaders";
-                this.appendToTable( columnName, value);
+                this.currentTable = "packetheaders";
+                this.appendToTable(columnName, value);
                 break;
             case MESSAGE_HEADER:
-                this.currentTable="messageheaders";
-                this.appendToTable( columnName, value);
+                this.currentTable = "messageheaders";
+                this.appendToTable(columnName, value);
                 break;
             case GROUP_HEADER:
-                this.currentTable="groupheaders";
+                this.currentTable = "groupheaders";
                 this.appendToTable(columnName, value);
                 break;
             case GROUP_ENTRIES:
-                this.currentTable=scopeTracker.getNonTerminalScope();
-    //            this.appendToResidual("GroupEntry\n");
-                this.appendToTable( columnName,value);
+                this.currentTable = scopeTracker.getNonTerminalScope();
+                //            this.appendToResidual("GroupEntry\n");
+                this.appendToTable(columnName, value);
                 break;
             case UNKNOWN:
                 break;
@@ -63,14 +63,16 @@ public class TablesHandler {
     }
 
     public void close() throws IOException {
-        for(SingleTableOutput singleTableOutput: singleTablesOutput.values()){
-           singleTableOutput.close();
+        for (SingleTableOutput singleTableOutput : singleTablesOutput.values()) {
+            singleTableOutput.close();
         }
+        this.singleTablesOutput.clear();
     }
-    public void startMessageHeader(String tokenName, int tokenId){
+
+    public void startMessageHeader(String tokenName, int tokenId) {
         this.scopeTracker.clear();
         this.scopeTracker.pushScope(tokenName);
-        this.scopeTracker.scopeLevel= MESSAGE_HEADER;
+        this.scopeTracker.scopeLevel = MESSAGE_HEADER;
         this.appendColumnValue("MessageId", String.valueOf(tokenId));
         this.appendColumnValue("MessageName", tokenName);
     }
@@ -78,21 +80,22 @@ public class TablesHandler {
     public void endMessageHeader(String tokenName) throws IOException {
         this.scopeTracker.pushScope(tokenName);
         this.singleTablesOutput.get("messageheaders").completeRow();
-        this.scopeTracker.scopeLevel= UNKNOWN;
+        this.scopeTracker.scopeLevel = UNKNOWN;
     }
 
-    public void beginGroupHeader(){
-        this.scopeTracker.scopeLevel= GROUP_HEADER;
+    public void beginGroupHeader() {
+        this.scopeTracker.scopeLevel = GROUP_HEADER;
     }
+
     public void endGroupHeader() throws IOException {
-        this.scopeTracker.scopeLevel= UNKNOWN;
+        this.scopeTracker.scopeLevel = UNKNOWN;
         this.singleTablesOutput.get("groupheaders").completeRow();
     }
 
     public void beginGroup(String tokenName) throws IOException {
-       this.scopeTracker.pushScope(tokenName);
-       this.addTable(this.scopeTracker.getNonTerminalScope());
-       this.scopeTracker.scopeLevel=ScopeLevel.GROUP_ENTRIES;
+        this.scopeTracker.pushScope(tokenName);
+        this.addTable(this.scopeTracker.getNonTerminalScope());
+        this.scopeTracker.scopeLevel = ScopeLevel.GROUP_ENTRIES;
     }
 
     public void endGroup() throws IOException {
@@ -101,17 +104,17 @@ public class TablesHandler {
     }
 
     public void setPacketValues(int message_size, long packet_sequence_number, long sendingTime) throws IOException {
-        this.scopeTracker.scopeLevel= PACKET_HEADER;
+        this.scopeTracker.scopeLevel = PACKET_HEADER;
         this.appendColumnValue("message_size", String.valueOf(message_size));
-        this.appendColumnValue("packet_sequence_number",  String.valueOf(packet_sequence_number));
+        this.appendColumnValue("packet_sequence_number", String.valueOf(packet_sequence_number));
         this.appendColumnValue("sendingTime", String.valueOf(sendingTime));
         this.completeRow("packetheaders");
-        this.scopeTracker.scopeLevel= UNKNOWN;
+        this.scopeTracker.scopeLevel = UNKNOWN;
     }
 
-   public void pushScope(String name) {
+    public void pushScope(String name) {
         this.scopeTracker.pushScope(name);
-   }
+    }
 
     public void popScope() {
         this.scopeTracker.popScope();
--- sbe-samples/src/main/java/uk/co/real_logic/sbe/read_cme_pcaps/readers/ReadPcaps.java
+++ sbe-samples/src/main/java/uk/co/real_logic/sbe/read_cme_pcaps/readers/ReadPcaps.java
@@ -9,6 +9,7 @@ import uk.co.real_logic.sbe.tests.DirectoryComparison;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.TreeMap;
 
 
 public class ReadPcaps {
@@ -16,30 +17,64 @@ public class ReadPcaps {
 
     public static void main(final String[] args) throws Exception {
 //        ReadPcapProperties prop = new ReadPcapProperties(args[0]);
-        String sourceConfig= "C:\\marketdata\\testdata\\configs\\multiplefiles.config";
+        String sourceConfig = "C:\\marketdata\\testdata\\configs\\multiplefiles.config";
         //todo: make multiple properties objects for different types of properties
         //todo: use different prefixes for these different objects. Pass only what is needed
         ReadPcapProperties prop = new ReadPcapProperties(sourceConfig);
-        ArrayList<String> inFiles = prop.inFiles;
 
-        for(String inFile: inFiles) {
-            runJob(prop, inFile);
-        }
+
+       ArrayList<String> inFiles=getFilesFromProperties(prop) ;
+        //todo: figure out why loop doesn't work when stated with colon
+        String filesPath = "C:\\marketdata\\testdata\\20191006\\";
+        inFiles=getFilesFromDirectoryName(filesPath);
+        runJob(prop, inFiles);
     }
 
-    private static void runJob(ReadPcapProperties prop, String inFile) throws Exception {
 
-        BinaryDataHandler binaryDataHandler = new BinaryDataHandler(prop, inFile);
+
+   private static ArrayList<String> getFilesFromProperties(ReadPcapProperties prop){
+       return prop.inFiles;
+   }
+
+    private static ArrayList<String> getFilesFromDirectoryName(String filesPath){
+//        File path = new File("C:\\marketdata\\testdata\\fulldaypcaps\\");
+        File[] listFiles = new File(filesPath).listFiles();
+        TreeMap orderedFilePaths = new TreeMap<Integer, String>();
+        ArrayList<String>  fileStrings = new ArrayList<>();
+        for (int i = 0; i < listFiles.length; i++){
+            String fileString =  listFiles[i].toString();
+            String[] splitString = fileString.split("[.]");
+            int lastDigit= Integer.parseInt(splitString[splitString.length-1]);
+            orderedFilePaths.put(lastDigit, fileString);
+            }
+        for (int i = 1; i <= listFiles.length; i++){
+            fileStrings.add(String.valueOf(orderedFilePaths.get(i)));
+        }
+
+        return fileStrings;
+
+    }
+
+
+    private static void runJob(ReadPcapProperties prop, ArrayList<String> inFiles) throws Exception {
 
         TablesHandler tablesHandler = new TablesHandler("C:\\marketdata\\testdata\\separatetables\\latestresults\\");
-        TokenListener cleanTokenListener = new CleanTokenListener(tablesHandler);
+        for (int i = 0; i < inFiles.size(); i++) {
+            String inFile = inFiles.get(i);
 
-        PacketReader packetReader =new PacketReader(prop, binaryDataHandler, tablesHandler);
-        packetReader.readPackets(cleanTokenListener);
-        packetReader.endPacketsCollection();
+
+            BinaryDataHandler binaryDataHandler = new BinaryDataHandler(prop, inFile);
+
+            TokenListener cleanTokenListener = new CleanTokenListener(tablesHandler);
+
+            PacketReader packetReader = new PacketReader(prop, binaryDataHandler, tablesHandler);
+            packetReader.readPackets(cleanTokenListener);
+
+
+        }
+        tablesHandler.close();
         //test directory comparison by comparing same directory
-        testRunEquality();
-
+        //testRunEquality();
     }
 
     private static void testRunEquality() throws IOException {
